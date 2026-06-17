ad0dd55f2e95169667e6add582cc6fcb8a5f4e84
Shreya Dandekar <shreya.dandekar1996@@gmail.com>
2026-06-17 17:48:08 +0530

    Added CV download button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,3 +1,25 @@
+// import React from "react";
+// import "./Hero.css";
+
+// const Hero = () => {
+//   return (
+//     <section className="hero">
+//       <div className="hero-content">
+//         <h1>Hi, I'm Shreya</h1>
+//         <p>Frontend Developer | React Developer</p>
+
+       
+//         <a href={`${process.env.PUBLIC_URL}/TShreya_Resume.pdf`} download>
+//           <button className="hero-btn">Download CV</button>
+//         </a>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Hero;
+
+
 import React from "react";
 import "./Hero.css";
 
@@ -8,9 +30,12 @@ const Hero = () => {
         <h1>Hi, I'm Shreya</h1>
         <p>Frontend Developer | React Developer</p>
 
-        {/* CV Download Button */}
-        <a href={`${process.env.PUBLIC_URL}/TShreya_Resume.pdf`} download>
-          <button className="hero-btn">Download CV</button>
+        <a
+          href={`${process.env.PUBLIC_URL}/TShreya_Resume.pdf`}
+          download="Shreya_Dandekar_Resume.pdf"
+         
+        >
+           <button className="hero-btn">Download CV</button>
         </a>
       </div>
     </section>
