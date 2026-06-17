75fccf5ea2fff13ab5acda63e4f827ac7d57737e
Shreya Dandekar <shreya.dandekar1996@@gmail.com>
2026-06-17 18:03:32 +0530

    Added CV download button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,6 @@
 //         <h1>Hi, I'm Shreya</h1>
 //         <p>Frontend Developer | React Developer</p>
 
-       
 //         <a href={`${process.env.PUBLIC_URL}/TShreya_Resume.pdf`} download>
 //           <button className="hero-btn">Download CV</button>
 //         </a>
@@ -18,7 +17,6 @@
 // };
 
 // export default Hero;
-
 
 import React from "react";
 import "./Hero.css";
@@ -33,7 +31,6 @@ const Hero = () => {
         <a
           href={`${process.env.PUBLIC_URL}/TShreya_Resume.pdf`}
           download="Shreya_Dandekar_Resume.pdf"
-          
         >
           <button className="hero-btn">Download CV</button>
         </a>
